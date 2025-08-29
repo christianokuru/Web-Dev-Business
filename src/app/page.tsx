ab69148ca594b77d8e5f9d31f1adfae24c7e8bce
christianokuru <okuruchristian@gmail.com>
2025-08-29 11:39:25 +0100

    folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
-import {Hero}  from "../components/pages/landing";
+import {HeroSection}  from "@/components/custom/non-reusable";
+import { CardComponent } from "../components/custom/reusable/CardComponent";
 export default function Home() {
   return (
     <>
-      <Hero/>
+      <HeroSection/>
+      <div className="flex justify-center mt-10">
+      <CardComponent/>
+      </div>
     </>
   );
 }
--- src/components/pages/landing/Hero.tsx
+++
@@ -1,8 +0,0 @@
-export function Hero() {
-    return (
-        <>
-            <h1>From /components/pages/landing/Hero</h1>
-            <h2>Don't forget to update the <mark>index.ts file</mark> in the individual page folders when you create <mark>new section components</mark></h2>
-        </>
-    )
-}
